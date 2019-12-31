66f66cda9e7e71819feada6a0c54183987f32168
EC2 Default User <ec2-user@ip-172-31-29-202.us-west-2.compute.internal>
2019-12-31 20:38:11 +0000

    Update routes

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const axios = require('axios')
 
 console.log('Starting up app')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    console.log('Hello requested')
-    res.send('Hello World!')
+app.get('/health', (req, res) => {
+    res.status(200).send('OK')
+})
+
+app.get('/public', (req, res) => {
+    const host = process.env.HOSTNAME
+    console.log('Hello requested - ${host}')
+    res.send(`Hello World from ${host}`)
+})
+
+app.get('/private', (req, res) => {
+    console.log('Requesting data from private API')
+    axios.get('private-api.local').then((result => {
+        res.send(result)
+    })).catch(err => {
+        console.error(err.message)
+        res.status(500).send(err.message)
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
